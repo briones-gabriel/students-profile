065757e6d2f58a4fa02ffb164ae026c5a55ba564
briones-gabriel <dossantosgabrielb@gmail.com>
2021-06-29 18:57:08 -0300

    Finish assesment part 1: Getting and showing data from the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+type Student = {
+  id: number,
+  city: string,
+  company: string,
+  email: string,
+  firstName: string,
+  lastName: string,
+  grades: Array<string>,
+  pic: string,
+  skill: string
+};
+
+type Data = {
+  students: Array<Student>
+}
 
 function App() {
+  const [students, setStudents] = useState<Array<Student>>([]);
+
+  useEffect(() => {
+    fetch("https://api.hatchways.io/assessment/students")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data: Data) => {
+        setStudents(data.students);
+      })
+      .catch((error) => {
+        throw Error("There was an error in the request: " + error);
+      });
+  }, [])
+
+  /**
+   * Returns the average of the sum of all the items in a given array of numbers.
+   *
+   * @param {Array<string>} list - The list of numbers.
+   * @return {number} The average of the sum of all the elements in the array.
+   */
+  const getAverage = (list: Array<string>): number => {
+    return list.reduce((a, b) => Number(a) + Number(b), 0)/list.length;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {students.map((student: Student) => (
+        <div key={student.id}>
+          <img src={student.pic} alt={student.firstName} />
+          <h3>{student.firstName + " " + student.lastName}</h3>
+          <p>Email: {student.email}</p>
+          <p>Company: {student.company}</p>
+          <p>Skill: {student.skill}</p>
+          <p>Average: {getAverage(student.grades)}</p>
+        </div>
+      ))}
     </div>
   );
 }
